17a101c99ba91162a6438434e8ab912fd7a2e005
Anka <runner@sjc22-bt149-8dc3b7fc-decb-4715-b995-cabedc73e91a-E646DC64E098.local>
2026-03-02 19:15:23 +0000

    Build: New: create spm support for 3.9.4-rc1

--- Package.swift
+++ Package.swift
@@ -5,17 +5,18 @@ import PackageDescription
 let package = Package(
   name: "Flybits",
   products: [
-    .library(name: "Concierge", targets: ["FlybitsCoreConcierge", "FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK", "FlybitsConcierge"]),
+    .library(name: "Concierge", targets: ["FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK", "FlybitsConciergeSDK"]),
     .library(name: "Location", targets: ["FlybitsSDK", "FlybitsContextSDK", "FlybitsContextLocationPluginSDK"]),
     .library(name: "Core", targets: ["FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK"]),
+    .library(name: "SmartRewards", targets: ["FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK", "FlybitsConciergeSDK", "FlybitsSmartRewardsSDK"]),
   ],
   targets: [
-    .binaryTarget(name: "FlybitsSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17-rc1/FlybitsSDK.xcframework.zip", checksum: "38401e5dc1b1d9c18b692aac3b93a096f3c74a262ffbfa409c8b10208c9688ec"),
-    .binaryTarget(name: "FlybitsPushSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17-rc1/FlybitsPushSDK.xcframework.zip", checksum: "c5eb349b57760ef2461b343717d47739716f4ff1ea999156cf99b235e24bd36c"),
-    .binaryTarget(name: "FlybitsKernelSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17-rc1/FlybitsKernelSDK.xcframework.zip", checksum: "9ad532fc984b3931cfd1b71f413d6908a52c5b5a8befbf5eeaa13afca97d6133"),
-    .binaryTarget(name: "FlybitsContextSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17-rc1/FlybitsContextSDK.xcframework.zip", checksum: "acce2afc74401fc8bf993da886bf6449f275513b17ce95ebbe9760631a296964"),
-    .binaryTarget(name: "FlybitsCoreConcierge", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17-rc1/FlybitsCoreConcierge.xcframework.zip", checksum: "0e2ae4697a75bc11893abbe8b7286f9e317ea1ed638feb3ba8cc19b69b3acbcf"),
-    .binaryTarget(name: "FlybitsConcierge", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17-rc1/FlybitsConcierge.xcframework.zip", checksum: "2927b9ff84da5115060058f0a3eea579e428352dcb01eaaa2cf9fb2efe6fdf14"),
-    .binaryTarget(name: "FlybitsContextLocationPluginSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17-rc1/FlybitsContextLocationPluginSDK.xcframework.zip", checksum: "fc4f9027951bb78ee654bd85b6f3ea19960005ad24ffe5c693e6736c4ad6f5d3"),
+    .binaryTarget(name: "FlybitsSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4-rc1/FlybitsSDK.xcframework.zip", checksum: "137d45339ed1a7c1b6627c2c132a4fc85dbb78c9f0daa402f8800f91b3407ff5"),
+    .binaryTarget(name: "FlybitsPushSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4-rc1/FlybitsPushSDK.xcframework.zip", checksum: "cc133aec7acddfbc82c761015ee9f9065fe4b5b116274fc4e96cdff1689049a9"),
+    .binaryTarget(name: "FlybitsKernelSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4-rc1/FlybitsKernelSDK.xcframework.zip", checksum: "b669517c4be6e75dcb44850d4ba6e366770ec33ab8f68dd41fcc83f10259685e"),
+    .binaryTarget(name: "FlybitsContextSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4-rc1/FlybitsContextSDK.xcframework.zip", checksum: "257bf32ba9a2e505f46c9d5574b43c1b2c371f4434608f580fdf93448cfb9155"),
+    .binaryTarget(name: "FlybitsConciergeSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4-rc1/FlybitsConciergeSDK.xcframework.zip", checksum: "0893a9e5860217f4f07adba3d139e297a6fe87fd07928d0e09bf29380fda0fb8"),
+    .binaryTarget(name: "FlybitsSmartRewardsSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4-rc1/FlybitsSmartRewardsSDK.xcframework.zip", checksum: "97591b260c2da225d7608e0490257cf203387fde242f220ecbed798049485a1a"),
+    .binaryTarget(name: "FlybitsContextLocationPluginSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4-rc1/FlybitsContextLocationPluginSDK.xcframework.zip", checksum: "0e824f912691e34dbde2f3bf774b1408ef24d594f1a690ff320bcdb57002c522"),
   ]
 )
