b5576cd84d1e8a13752e0a6741a0960cba9a7319
Anka <runner@sjc22-bm206-3d977e5e-f0cf-4b98-9508-f8eaad569791-067434AA2BD0.local>
2026-03-03 22:31:05 +0000

    Build: New: create spm support for 5.0.17

--- Package.swift
+++ Package.swift
@@ -5,18 +5,17 @@ import PackageDescription
 let package = Package(
   name: "Flybits",
   products: [
-    .library(name: "Concierge", targets: ["FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK", "FlybitsConciergeSDK"]),
+    .library(name: "Concierge", targets: ["FlybitsCoreConcierge", "FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK", "FlybitsConcierge"]),
     .library(name: "Location", targets: ["FlybitsSDK", "FlybitsContextSDK", "FlybitsContextLocationPluginSDK"]),
     .library(name: "Core", targets: ["FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK"]),
-    .library(name: "SmartRewards", targets: ["FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK", "FlybitsConciergeSDK", "FlybitsSmartRewardsSDK"]),
   ],
   targets: [
-    .binaryTarget(name: "FlybitsSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.8.7/FlybitsSDK.xcframework.zip", checksum: "eaea9f3f8f25ae870b7cd0e1cd718639c7b7d4872f98b78aab80dd802800ca6e"),
-    .binaryTarget(name: "FlybitsPushSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.8.7/FlybitsPushSDK.xcframework.zip", checksum: "423104ccf2e6ac264d96bb6d7288e7e4fd37b339168e76663aa0eccb3bcd77a0"),
-    .binaryTarget(name: "FlybitsKernelSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.8.7/FlybitsKernelSDK.xcframework.zip", checksum: "bdafd9a752e8ba99826c53d347466dc5055959887e5298952a41d2a55f4b1e7b"),
-    .binaryTarget(name: "FlybitsContextSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.8.7/FlybitsContextSDK.xcframework.zip", checksum: "7809f6921cebe517846665aa7060024e1f7f35c04bbd45c032311a2ad0caf309"),
-    .binaryTarget(name: "FlybitsConciergeSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.8.7/FlybitsConciergeSDK.xcframework.zip", checksum: "39571db373f4fcc092e6f9ca24f59e3f4154b3eb40aa01287be0968878ca6e5f"),
-    .binaryTarget(name: "FlybitsSmartRewardsSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.8.7/FlybitsSmartRewardsSDK.xcframework.zip", checksum: "c58c19643210ade5cad634bcb8df97569b3eb7527dde21288c42bfef538cb598"),
-    .binaryTarget(name: "FlybitsContextLocationPluginSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.8.7/FlybitsContextLocationPluginSDK.xcframework.zip", checksum: "3e4e618ae10b04d8845c0ad03ce286adb9629e7d394fd87a80214e84b8a0b575"),
+    .binaryTarget(name: "FlybitsSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17/FlybitsSDK.xcframework.zip", checksum: "067bd3e84af20ac4b077c67ff9b02cbb64ffbf4000fdfb06fefb7981cb262933"),
+    .binaryTarget(name: "FlybitsPushSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17/FlybitsPushSDK.xcframework.zip", checksum: "2fc83d7d59831690acf90c749a180b43f481c96cc77a954972265ce2cb418b0c"),
+    .binaryTarget(name: "FlybitsKernelSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17/FlybitsKernelSDK.xcframework.zip", checksum: "1590820f0431e539a8948eacbe58c0c4fec9dbd88432531ec6507c045f3b4e6e"),
+    .binaryTarget(name: "FlybitsContextSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17/FlybitsContextSDK.xcframework.zip", checksum: "cf2adb4814f5ffda0b53e0ee0162d32c6bc4a6b2a5d0831a51163bc03319c529"),
+    .binaryTarget(name: "FlybitsCoreConcierge", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17/FlybitsCoreConcierge.xcframework.zip", checksum: "c6947f2235407ceb463177a2c23785ababff145a1ac73de3d59a56e4530e950d"),
+    .binaryTarget(name: "FlybitsConcierge", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17/FlybitsConcierge.xcframework.zip", checksum: "2a30aa960c5056c2ebb5c7646aa319782a91db9b650cc28737b07ff859798365"),
+    .binaryTarget(name: "FlybitsContextLocationPluginSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.0.17/FlybitsContextLocationPluginSDK.xcframework.zip", checksum: "8c9bf0183f4f1d0187b2572c8058bae843d8986e9bb9950bdd4a4f0bd7945de4"),
   ]
 )
