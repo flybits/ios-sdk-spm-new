52cbeb4f3e4f2123544fe6881d64a10c56627db8
Anka <runner@iad20-fj922-bf338751-6bc7-404f-828d-a31f41a93cb7-CE30DC6D0E60.local>
2026-03-30 19:44:44 +0000

    Build: New: create spm support for 5.1.8-rc1

--- Package.swift
+++ Package.swift
@@ -5,18 +5,17 @@ import PackageDescription
 let package = Package(
   name: "Flybits",
   products: [
-    .library(name: "Concierge", targets: ["FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK", "FlybitsConciergeSDK"]),
+    .library(name: "Concierge", targets: ["FlybitsCoreConcierge", "FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK", "FlybitsConcierge"]),
     .library(name: "Location", targets: ["FlybitsSDK", "FlybitsContextSDK", "FlybitsContextLocationPluginSDK"]),
     .library(name: "Core", targets: ["FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK"]),
-    .library(name: "SmartRewards", targets: ["FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK", "FlybitsConciergeSDK", "FlybitsSmartRewardsSDK"]),
   ],
   targets: [
-    .binaryTarget(name: "FlybitsSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4/FlybitsSDK.xcframework.zip", checksum: "c76ee01a38883332e0110875e29830d9371d6d577663b0a74b485eefb94d6750"),
-    .binaryTarget(name: "FlybitsPushSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4/FlybitsPushSDK.xcframework.zip", checksum: "b735d68e17a38972ebfc1bc75ffdc46d0d6bf92a23dfaf4a5a15e23972308db9"),
-    .binaryTarget(name: "FlybitsKernelSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4/FlybitsKernelSDK.xcframework.zip", checksum: "76b21a33ccbc802f93d5f47dbff3830d49e2f8bdf52c0f1e7834c0069535a67b"),
-    .binaryTarget(name: "FlybitsContextSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4/FlybitsContextSDK.xcframework.zip", checksum: "93e98f3eb47760c120939e1d6cf29aaa0bc1157bd3aee31d334587182b48b97d"),
-    .binaryTarget(name: "FlybitsConciergeSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4/FlybitsConciergeSDK.xcframework.zip", checksum: "4824085a1297d60caa1a6958f1d38b118a3e825c0c1b43e761cc0158fd85a739"),
-    .binaryTarget(name: "FlybitsSmartRewardsSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4/FlybitsSmartRewardsSDK.xcframework.zip", checksum: "c5ab810beafb29fe2ba7aad72f0e80866dd75e92e012bf44c3b30eb0bf430aea"),
-    .binaryTarget(name: "FlybitsContextLocationPluginSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/3.9.4/FlybitsContextLocationPluginSDK.xcframework.zip", checksum: "6fdb2113435a7d793ca3beaae9351702b9605f6b7c37959459052822566be8b4"),
+    .binaryTarget(name: "FlybitsSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.1.8-rc1/FlybitsSDK.xcframework.zip", checksum: "3202bca9aa6eb6edf0bfef5d89a8fc3ba70e03c0027feb470ceffa63a9ef993b"),
+    .binaryTarget(name: "FlybitsPushSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.1.8-rc1/FlybitsPushSDK.xcframework.zip", checksum: "c3a5e490aa4f519603a0b62a0a647d13684d68b339e9f72b88be6454736d4370"),
+    .binaryTarget(name: "FlybitsKernelSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.1.8-rc1/FlybitsKernelSDK.xcframework.zip", checksum: "a8fbcea645b1c8608f7736c56e7a573b4c2bc870ec68cfc126e1cf4ec3014bd6"),
+    .binaryTarget(name: "FlybitsContextSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.1.8-rc1/FlybitsContextSDK.xcframework.zip", checksum: "b5da30873e3e8f54eb3e12a461ecf169e6f40c8d9740e61a224bdcd3d99e3240"),
+    .binaryTarget(name: "FlybitsCoreConcierge", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.1.8-rc1/FlybitsCoreConcierge.xcframework.zip", checksum: "948d5fdcd18fa55020b8fc184a58906f216359def5f05b20b2a0e330d946c78b"),
+    .binaryTarget(name: "FlybitsConcierge", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.1.8-rc1/FlybitsConcierge.xcframework.zip", checksum: "7b79b6c2bae58fc045d4232f511538b723edd5efa789a6b423478f68a7cc0a52"),
+    .binaryTarget(name: "FlybitsContextLocationPluginSDK", url: "https://sdks.flybits.com/ios/SPM/Flybits/5.1.8-rc1/FlybitsContextLocationPluginSDK.xcframework.zip", checksum: "12864c399d9d01e3ea427402b862753b3b3b88eff5d3f3fc5ea7ca8bf2acc21b"),
   ]
 )
